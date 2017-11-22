4c51bebd8f5e93bebc6a7db46ef0397d55a2363b
Levi Donaldson <lcdbacanas@gmail.com>
2017-11-21 19:28:09 -0600

    Create jsArrays.js

---
+++ jsArrays.js
@@ -0,0 +1,30 @@
+window.onload = function () {
+    
+    let list = [];
+
+    function addPassenger(name, list = []) {
+        if (list.length == 0){
+            list.push(name);
+            return list;
+        } else {
+            for(var i = 0; i < list.length; i++){
+                if (list[i] == undefined){
+                    list[i] = name;
+                    return list;
+                } else if (i == list.length - 1) {
+                    list.push(name);
+                    return list;
+                }
+            }
+        }
+        
+    }
+
+    list = addPassenger("levi", list);
+    list = addPassenger(" katie", list);
+    list = addPassenger(" mackenzie", list);
+    list = addPassenger(" ashlyn", list);
+    list = addPassenger(" mailey", list);
+
+    document.getElementById("listHere").innerHTML = list;
+}
