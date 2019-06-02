3ea52dbd9a41f7299f6d4edb3f9673d8a4b3399f
Levi Donaldson <lcdbacanas@gmail.com>
2019-06-01 20:50:53 -0500

    Create reactCounter.js

---
+++ reactCounter.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * from '../style/main.less';
+
+export default class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0
+    }
+    this.inc = this.inc.bind(this);
+  }
+
+  inc = () => {
+    this.setState({
+      value: this.state.value + 1
+    });
+  };
+
+  dec = () => {
+    this.setState({
+      value: this.state.value - 1
+    });
+  };
+
+  render() {
+    return (
+        <div className="divStyle">
+          <button className="btnLayout btnSkin dec" onClick={this.dec}> - </button>
+          <div className="counterWrapper">
+            <p className="counterTextLayout">{this.state.value}</p>
+          </div>
+          <button className="btnLayout btnSkin inc" onClick={this.inc}> + </button>
+        </div>
+    );
+  }
+}
+
+ReactDOM.render(<Counter />, document.getElementById('root'));
